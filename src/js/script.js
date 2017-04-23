aeff60a19f5be46eae7902028f5aed59e86efeb3
Nacho Lozano <nlozano@auge.digital>
2017-04-23 17:29:29 +0200

    Tab reload
    - Cant scroll down while pulling to refresh

--- src/js/script.js
+++ src/js/script.js
@@ -151,9 +151,12 @@ function moveRefresh(e){
         dom.tabReloaderIcon.classList.remove('ready-for-reload');
       }
 
+    }else{
+      state.refreshing = false;
     }
   }else{
     refresh.startPoint = null;
+    state.refreshing = false;
   }
 }
 
